6d72cab5cb7d080e4c9d29fefb754b53458138ce
Eikcaj.1998 <jw220998@gmail.com>
2022-09-26 18:24:35 +0200

    esercizio finito

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\Admin\HomeController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
